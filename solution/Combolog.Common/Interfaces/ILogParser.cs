4e7b7372c731bd5ca01db39b8851003e71f302af
udragan <ujfa2001@yahoo.com>
2018-01-08 19:52:52 +0100

    added ComboLog.Common project.

---
+++ solution/Combolog.Common/Interfaces/ILogParser.cs
@@ -0,0 +1,16 @@
+﻿using com.udragan.csharp.ComboLog.Model.Models;
+
+namespace com.udragan.csharp.Combolog.Common.Interfaces
+{
+	/// <summary>
+	/// Interface for log parsers.
+	/// </summary>
+	public interface ILogParser
+	{
+		/// <summary>
+		/// Gets the "one line" of log.
+		/// </summary>
+		/// <returns>Log entry.</returns>
+		LogEntryModel GetEntry();
+	}
+}
