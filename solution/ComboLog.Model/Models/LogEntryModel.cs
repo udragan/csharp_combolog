d305f0a2ed89c83a9c05214f03ed1cc146fd7dfe
udragan <ujfa2001@yahoo.com>
2018-01-08 18:43:09 +0100

    added Model project.

---
+++ solution/ComboLog.Model/Models/LogEntryModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace com.udragan.csharp.ComboLog.Model.Models
+{
+	/// <summary>
+	/// Model representing one log entry.
+	/// </summary>
+	public class LogEntryModel
+	{
+		#region Constructors
+
+		public LogEntryModel(DateTime timestamp, string value)
+		{
+			Timestamp = timestamp;
+			Value = value;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the timestamp of log entry.
+		/// </summary>
+		public DateTime Timestamp { get; private set; }
+
+		/// <summary>
+		/// Gets the value of log entry.
+		/// </summary>
+		public string Value { get; private set; }
+
+		#endregion
+	}
+}
